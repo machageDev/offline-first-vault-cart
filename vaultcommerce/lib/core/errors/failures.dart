ee0b7a207884512ed852b0b181ca46820ee158c3
machageDev <machagefranklyn@gmail.com>
2026-06-28 23:02:42 +0300

    designrd core handling matrix

---
+++ vaultcommerce/lib/core/errors/failures.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  List<Object> get props => [message];
+
+  class ServerFailure extends Failure {
+    const ServerFailure(String message) : super(message);
+  }
+  class CacheFailure extends Failure {
+    const CacheFailure(String message) : super(message);
+  }
+  class SecurityTemperingFailure extends Failure {
+    const SecurityTemperingFailure(String message) : super(message);
+  }
+  class AuthenticationFailure extends Failure {
+    const AuthenticationFailure(String message) : super(message);
+  }
+}
