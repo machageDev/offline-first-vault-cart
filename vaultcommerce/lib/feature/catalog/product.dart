21e5b111c1c8205fa6e32a068b2bccc8b95520e8
machageDev <machagefranklyn@gmail.com>
2026-07-01 16:24:47 +0300

    designed core business model for product catalog

---
+++ vaultcommerce/lib/feature/catalog/product.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final int currentStock;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.currentStock,
+  });
+
+  @override
+  List<Object?> get props => [id, name, description, price, imageUrl];
+}
